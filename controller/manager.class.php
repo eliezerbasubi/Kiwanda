a9d29958be54b884ff199947b53e463745ed1fc0
Domeshow <domeshowemmanuel@gmail.com>
2020-02-03 16:23:48 +0200

    ch(dev): structuration du projet [Start #171051692]

---
+++ controller/manager.class.php
@@ -0,0 +1,12 @@
+<?php
+require('model/model.class.php');
+require('model/users.class.php');
+
+function home_page()
+{
+    require('view/home.php');
+}
+function login_page()
+{
+    require('view/login.php');
+}
